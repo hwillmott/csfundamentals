a349fcd93aea467b6cdc60f23d96fb23d776ae1e
Harriet Willmott <harriet.willmott@gmail.com>
2019-09-23 10:50:51 -0700

    simpler solution

--- leetcode/009_palindromenumber.py
+++ leetcode/009_palindromenumber.py
@@ -4,14 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0: return False
-        if x < 10: return True
-        if x % 10 == 0: return False
-        
-        v = 0
-        while x - v > 0:
-            v = v * 10 + x % 10
-            x = x / 10
-        if v > x:
-            v = v / 10
-        return v == x
+        if x < 0:
+            return False
+        start = x
+        result = 0
+        while x:
+            result *= 10
+            result += x%10
+            x /= 10
+        return start == result
