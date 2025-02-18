412e05f486cf2b97786014969cdf719b79fe786c
Harriet Willmott <harriet.willmott@gmail.com>
2025-02-18 11:58:57 -0800

    Update 002_addtwonumbers.ts

--- leetcode/002_addtwonumbers.ts
+++ leetcode/002_addtwonumbers.ts
@@ -1,32 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        curr1 = l1
-        curr2 = l2
-        
-        result = ListNode(0)
-        curr3 = result
-        s = 0
-        while curr1 is not None or curr2 is not None:
-            s /= 10
-            if curr1 is not None:
-                s += curr1.val
-                curr1 = curr1.next
-            if curr2 is not None:
-                s += curr2.val
-                curr2 = curr2.next
-            curr3.next = ListNode(s%10)
-            curr3 = curr3.next
-        if s/10 == 1:
-            curr3.next = ListNode(1)
-        return result.next
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let c1 = l1
+    let c2 = l2 
+    let result: ListNode = new ListNode()
+    let r = result
+    let carry = 0
+    while (c1 !== null || c2 !== null || carry) {
+        const sum = (c1 ? c1.val : 0) + (c2 ? c2.val : 0) + carry
+        const remainder = sum % 10
+        carry = sum / 10 >= 1 ? 1 : 0
+        r.next = new ListNode(remainder)
+        r = r.next
+        if (c1) c1 = c1.next
+        if (c2) c2 = c2.next
+    }
+    return result.next
+};
