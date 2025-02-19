bb1cce8b4146275d2387280a14bcfadc1c366b6f
Harriet Willmott <harriet.willmott@gmail.com>
2025-02-19 10:49:09 -0800

    Update 187_repeateddnasequences.ts

--- leetcode/187_repeateddnasequences.ts
+++ leetcode/187_repeateddnasequences.ts
@@ -19,3 +19,22 @@ function findRepeatedDnaSequences(s: string): string[] {
     })
     return results
 };
+
+function findRepeatedDnaSequences(s: string): string[] {
+    const counts: {[id: string]: number} = {}
+    if (s.length < 10) return []
+    let l = 0
+    let r = 9
+    while (r < s.length) {
+        const sub = s.substring(l, r+1)
+        counts[sub] = counts[sub] ? counts[sub] + 1 : 1
+        l = l + 1
+        r = r + 1
+    }
+
+    const results = []
+    for (const [key, value] of Object.entries(counts)) {
+        if (value > 1) results.push(key)
+    } 
+    return results
+};
