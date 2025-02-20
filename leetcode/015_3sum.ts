e7545ec6a7b60ea7ed8a04fb617a4cea15708583
Harriet Willmott <harriet.willmott@gmail.com>
2025-02-19 17:51:43 -0800

    Update 015_3sum.ts

--- leetcode/015_3sum.ts
+++ leetcode/015_3sum.ts
@@ -33,3 +33,36 @@ function threeSum(nums: number[]): number[][] {
     }
     return result
 };
+
+function threeSum(nums: number[]): number[][] {
+    nums.sort(function(a,b){return a-b})
+
+    const res = []
+
+    let i = 0
+    while (i < nums.length - 2) {
+        if (i > 0 && nums[i] === nums[i-1]) {
+            i = i + 1
+            continue
+        }
+
+        let j = i + 1
+        let k = nums.length - 1
+        while (j < k) {
+            const s = nums[i] + nums[j] + nums[k]
+            if (s === 0) {
+                res.push([nums[i], nums[j], nums[k]])
+                j = j + 1
+                while (j < k && nums[j] === nums[j-1]) {
+                    j = j + 1
+                }
+            } else if (s < 0) {
+                j = j + 1
+            } else {
+                k = k - 1
+            }
+        }
+        i = i + 1
+    }
+    return res
+};
