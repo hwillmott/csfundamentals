21772d270d8bbe14fbb1d5bd7929d474fbd95da3
Harriet Willmott <harriet.willmott@gmail.com>
2025-03-07 13:03:52 -0800

    Update 226_invertbinarytree.ts

--- leetcode/226_invertbinarytree.ts
+++ leetcode/226_invertbinarytree.ts
@@ -12,16 +12,26 @@
  * }
  */
 
+// function invertTree(root: TreeNode | null): TreeNode | null {
+//     const q = [root]
+//     while (q.length) {
+//         const curr = q.shift()
+//         if (!curr) continue
+//         const temp = curr.left
+//         curr.left = curr.right
+//         curr.right = temp
+//         q.push(curr.left)
+//         q.push(curr.right) 
+//     }
+//     return root
+// };
+
 function invertTree(root: TreeNode | null): TreeNode | null {
-    const q = [root]
-    while (q.length) {
-        const curr = q.shift()
-        if (!curr) continue
-        const temp = curr.left
-        curr.left = curr.right
-        curr.right = temp
-        q.push(curr.left)
-        q.push(curr.right) 
-    }
+    if (!root) return root
+    const temp = root.left
+    root.left = root.right
+    root.right = temp 
+    invertTree(root.left)
+    invertTree(root.right)
     return root
-};
+}
